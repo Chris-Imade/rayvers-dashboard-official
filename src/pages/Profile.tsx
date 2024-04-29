3a65eb2163a91f608d279c5907ed41e6eb9e25af
@Chrisdevcode <jephthahimade@gmail.com>
2024-04-29 18:56:27 +0100

    update profile - status=debugging

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,19 +3,53 @@ import DefaultLayout from '../layout/DefaultLayout';
 import CoverOne from '../images/cover/cover-01.png';
 import userSix from '../images/user/user-06.png';
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { CSSProperties, useEffect, useState } from 'react';
 import { BASE_URL } from '../components/DEFAULTS';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../Redux/store';
 import { setProfile } from '../Redux/Splice/AppSplice';
+import { ClipLoader } from 'react-spinners';
+
+const override: CSSProperties = {
+  display: 'block',
+  margin: '0 auto',
+  borderColor: 'white',
+};
 
 const Profile = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<any | null>(null);
+  const [editProfile, setEditProfile] = useState<boolean>(false);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [dragging, setDragging] = useState<boolean>(false);
+  const [name, setName] = useState<string>('');
+  const [bio, setBio] = useState<string>('');
+  const [date, setDate] = useState<Date | null>(null);
+
+  console.log('file: ', selectedFile);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
+
+  const handleDrop = (event: React.DragEvent<HTMLLabelElement>) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
+
+  const handleDragOver = (event: React.DragEvent<HTMLLabelElement>) => {
+    event.preventDefault();
+  };
 
   const profile = useSelector((state: RootState) => state.data.profile);
   const userProfile = useSelector((state: RootState) => state.data.userInfo);
-  console.log('profile: ', userProfile);
+  console.log('profile: ', profile);
   // Redux
   const token = useSelector((state: RootState) => state.data.token);
 
@@ -54,6 +88,50 @@ const Profile = () => {
     fetchProfile();
   }, []);
 
+  const updateProfile = async () => {
+    setLoading(true);
+
+    try {
+      const formData = new FormData();
+      if (name.length) {
+        formData.append('name', name);
+      } else if (date) {
+        formData.append('date_of_birth', date.toISOString());
+      } else if (bio.length) {
+        formData.append('bio', bio);
+      } else if (selectedFile) {
+        formData.append('profile_picture', selectedFile);
+      }
+
+      const response = await fetch(`${BASE_URL}auth/users/me/`, {
+        method: 'PUT',
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+        body: formData,
+      });
+
+      if (response.ok) {
+        // Handle success if needed
+        const userInfo = await response.json();
+        console.log(userInfo);
+        dispatch(setProfile(userInfo));
+        setLoading(false);
+      } else {
+        const errRes = await response.json();
+        console.log('Error: ', errRes);
+      }
+    } catch (error: any) {
+      // Handle error if needed
+      console.log(error);
+      setLoading(false);
+      setError(`Error updating profile: ${error.message}`);
+      console.log(`Error updating profile: ${error.message}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <DefaultLayout>
       <Breadcrumb pageName="Profile" />
@@ -65,75 +143,18 @@ const Profile = () => {
             alt="profile cover"
             className="h-full w-full rounded-tl-sm rounded-tr-sm object-cover object-center"
           />
-          <div className="absolute bottom-1 right-1 z-10 xsm:bottom-4 xsm:right-4">
-            <label
-              htmlFor="cover"
-              className="flex cursor-pointer items-center justify-center gap-2 rounded bg-primary py-1 px-2 text-sm font-medium text-white hover:bg-opacity-90 xsm:px-4"
-            >
-              <input type="file" name="cover" id="cover" className="sr-only" />
-              <span>
-                <svg
-                  className="fill-current"
-                  width="14"
-                  height="14"
-                  viewBox="0 0 14 14"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M4.76464 1.42638C4.87283 1.2641 5.05496 1.16663 5.25 1.16663H8.75C8.94504 1.16663 9.12717 1.2641 9.23536 1.42638L10.2289 2.91663H12.25C12.7141 2.91663 13.1592 3.101 13.4874 3.42919C13.8156 3.75738 14 4.2025 14 4.66663V11.0833C14 11.5474 13.8156 11.9925 13.4874 12.3207C13.1592 12.6489 12.7141 12.8333 12.25 12.8333H1.75C1.28587 12.8333 0.840752 12.6489 0.512563 12.3207C0.184375 11.9925 0 11.5474 0 11.0833V4.66663C0 4.2025 0.184374 3.75738 0.512563 3.42919C0.840752 3.101 1.28587 2.91663 1.75 2.91663H3.77114L4.76464 1.42638ZM5.56219 2.33329L4.5687 3.82353C4.46051 3.98582 4.27837 4.08329 4.08333 4.08329H1.75C1.59529 4.08329 1.44692 4.14475 1.33752 4.25415C1.22812 4.36354 1.16667 4.51192 1.16667 4.66663V11.0833C1.16667 11.238 1.22812 11.3864 1.33752 11.4958C1.44692 11.6052 1.59529 11.6666 1.75 11.6666H12.25C12.4047 11.6666 12.5531 11.6052 12.6625 11.4958C12.7719 11.3864 12.8333 11.238 12.8333 11.0833V4.66663C12.8333 4.51192 12.7719 4.36354 12.6625 4.25415C12.5531 4.14475 12.4047 4.08329 12.25 4.08329H9.91667C9.72163 4.08329 9.53949 3.98582 9.4313 3.82353L8.43781 2.33329H5.56219Z"
-                    fill="white"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M6.99992 5.83329C6.03342 5.83329 5.24992 6.61679 5.24992 7.58329C5.24992 8.54979 6.03342 9.33329 6.99992 9.33329C7.96642 9.33329 8.74992 8.54979 8.74992 7.58329C8.74992 6.61679 7.96642 5.83329 6.99992 5.83329ZM4.08325 7.58329C4.08325 5.97246 5.38909 4.66663 6.99992 4.66663C8.61075 4.66663 9.91659 5.97246 9.91659 7.58329C9.91659 9.19412 8.61075 10.5 6.99992 10.5C5.38909 10.5 4.08325 9.19412 4.08325 7.58329Z"
-                    fill="white"
-                  />
-                </svg>
-              </span>
-              <span>Edit</span>
-            </label>
-          </div>
         </div>
         <div className="px-4 pb-6 text-center lg:pb-8 xl:pb-11.5">
           <div className="relative z-30 mx-auto -mt-22 h-30 w-full max-w-30 rounded-full bg-white/20 p-1 backdrop-blur sm:h-44 sm:max-w-44 sm:p-3">
             <div className="relative drop-shadow-2">
-              <img src={userSix} alt="profile" />
-              <label
-                htmlFor="profile"
-                className="absolute bottom-0 right-0 flex h-8.5 w-8.5 cursor-pointer items-center justify-center rounded-full bg-primary text-white hover:bg-opacity-90 sm:bottom-2 sm:right-2"
-              >
-                <svg
-                  className="fill-current"
-                  width="14"
-                  height="14"
-                  viewBox="0 0 14 14"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M4.76464 1.42638C4.87283 1.2641 5.05496 1.16663 5.25 1.16663H8.75C8.94504 1.16663 9.12717 1.2641 9.23536 1.42638L10.2289 2.91663H12.25C12.7141 2.91663 13.1592 3.101 13.4874 3.42919C13.8156 3.75738 14 4.2025 14 4.66663V11.0833C14 11.5474 13.8156 11.9925 13.4874 12.3207C13.1592 12.6489 12.7141 12.8333 12.25 12.8333H1.75C1.28587 12.8333 0.840752 12.6489 0.512563 12.3207C0.184375 11.9925 0 11.5474 0 11.0833V4.66663C0 4.2025 0.184374 3.75738 0.512563 3.42919C0.840752 3.101 1.28587 2.91663 1.75 2.91663H3.77114L4.76464 1.42638ZM5.56219 2.33329L4.5687 3.82353C4.46051 3.98582 4.27837 4.08329 4.08333 4.08329H1.75C1.59529 4.08329 1.44692 4.14475 1.33752 4.25415C1.22812 4.36354 1.16667 4.51192 1.16667 4.66663V11.0833C1.16667 11.238 1.22812 11.3864 1.33752 11.4958C1.44692 11.6052 1.59529 11.6666 1.75 11.6666H12.25C12.4047 11.6666 12.5531 11.6052 12.6625 11.4958C12.7719 11.3864 12.8333 11.238 12.8333 11.0833V4.66663C12.8333 4.51192 12.7719 4.36354 12.6625 4.25415C12.5531 4.14475 12.4047 4.08329 12.25 4.08329H9.91667C9.72163 4.08329 9.53949 3.98582 9.4313 3.82353L8.43781 2.33329H5.56219Z"
-                    fill=""
-                  />
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M7.00004 5.83329C6.03354 5.83329 5.25004 6.61679 5.25004 7.58329C5.25004 8.54979 6.03354 9.33329 7.00004 9.33329C7.96654 9.33329 8.75004 8.54979 8.75004 7.58329C8.75004 6.61679 7.96654 5.83329 7.00004 5.83329ZM4.08337 7.58329C4.08337 5.97246 5.38921 4.66663 7.00004 4.66663C8.61087 4.66663 9.91671 5.97246 9.91671 7.58329C9.91671 9.19412 8.61087 10.5 7.00004 10.5C5.38921 10.5 4.08337 9.19412 4.08337 7.58329Z"
-                    fill=""
-                  />
-                </svg>
-                <input
-                  type="file"
-                  name="profile"
-                  id="profile"
-                  className="sr-only"
-                />
-              </label>
+              <img
+                src={
+                  profile?.profile_picture?.includes('https')
+                    ? profile.profile_picture
+                    : userSix
+                }
+                alt="profile"
+              />
             </div>
           </div>
           <div className="mt-4">
@@ -328,6 +349,87 @@ const Profile = () => {
               </div>
             </div>
           </div>
+          {/* Edit Profile Section */}
+          <button
+            onClick={() => {
+              setEditProfile((prevState) => !prevState);
+            }}
+            className="w-full py-3 border mt-8"
+          >
+            Edit Profile
+          </button>
+
+          {/* Edit Profile Form Below */}
+          {editProfile && (
+            <div className="mt-12">
+              <div
+                className={`bg-primary w-full h-[10rem] rounded-md flex justify-center items-center ${dragging && 'border-[3px] border-dashed border-white'}`}
+              >
+                <label
+                  className="h-full w-full flex justify-center items-center p-12"
+                  htmlFor="file"
+                  onDrop={handleDrop}
+                  onDragOver={handleDragOver}
+                  onDragEnter={() => setDragging(true)}
+                  onDragExit={() => setDragging(false)}
+                >
+                  <input
+                    id="file"
+                    type="file"
+                    onChange={handleFileChange}
+                    style={{ display: 'none' }}
+                  />
+                  {selectedFile ? (
+                    <p>Selected file: {selectedFile.name}</p>
+                  ) : (
+                    <p>Drag & drop your file here, or click to select</p>
+                  )}
+                </label>
+              </div>
+
+              <div className="mt-12">
+                <input
+                  className="text-slate-800 placeholder:text-slate-700 w-1/2 pl-4 py-4 border-r-4 outline-none"
+                  type="text"
+                  placeholder="Your name"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                />
+                <input
+                  className="text-slate-800 w-1/2 pl-4 py-4 outline-none"
+                  type="date"
+                  value={date ? date.toISOString().split('T')[0] : ''}
+                  onChange={(e) => setDate(new Date(e.target.value))}
+                />
+                <textarea
+                  className="text-slate-800 placeholder:text-slate-700 w-full pl-4 py-4 border-t-4 outline-none min-h-[10rem]"
+                  placeholder="Bio"
+                  onChange={(e) => setBio(e.target.value)}
+                  value={bio}
+                />
+              </div>
+
+              <div>
+                <button
+                  onClick={updateProfile}
+                  className="w-full py-4 bg-primary text-white mt-4 rounded-md"
+                >
+                  {loading ? (
+                    <ClipLoader
+                      color={'#ffffff'}
+                      loading={loading}
+                      cssOverride={override}
+                      size={30}
+                      aria-label="Loading Spinner"
+                      data-testid="loader"
+                    />
+                  ) : (
+                    'Update Profile'
+                  )}
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </DefaultLayout>
